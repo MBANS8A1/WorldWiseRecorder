dacb031c8b5e265926fdba880d932b953b432c8b
MBANS8A1 <seanmbng@aol.com>
2025-11-24 14:57:30 +0000

    I have included the PageNav instance within all the pages (Homepage, PageNotFound,Pricing and Product).

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
 function Homepage() {
   return (
     <div>
+      <PageNav />
       <h1>WorldWise</h1>
 
       <Link to="/pricing">Pricing</Link>
