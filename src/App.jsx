4f41db5fa784fdc2122a05f584d30951d3022f24
MBANS8A1 <seanmbng@aol.com>
2025-11-27 17:16:18 +0000

    I added a Route with a path attribute to "/login" in App.jsx, which holds all the Routes in the BrowserRouter (provided by React-Router).

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Pricing from "./pages/Pricing.jsx";
 import Homepage from "./pages/Homepage.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import AppLayout from "./pages/AppLayout.jsx";
+import Login from "./pages/Login.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
         <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
