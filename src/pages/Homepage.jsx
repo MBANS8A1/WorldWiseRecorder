40427aaa51029053b77a3480bcccb2243f717c67
MBANS8A1 <seanmbng@aol.com>
2025-11-27 11:57:14 +0000

    On the Homepage,jsx below the h2 element in the <section> I added a React-Router  Link element, which will link to the App page. It has the text "Start tracking now" which will be converted to uppercase and will be like a button element, which is inline-block; I added the global CSS className of .cta with the :link and :visited pseudoclasses.

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,5 @@
 import styles from "./Homepage.module.css";
+import { Link } from "react-router-dom";
 
 export default function Homepage() {
   return (
@@ -15,6 +16,9 @@ export default function Homepage() {
           of. Never forget your wonderful experiences, and show your friends how
           you have wandered the world when curioisty arrives.
         </h2>
+        <Link to="/app" className="cta">
+          Start tracking now
+        </Link>
       </section>
     </main>
   );
