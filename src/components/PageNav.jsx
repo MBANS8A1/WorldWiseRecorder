872670b9f115e9a67abfebb09c873498f019d5c2
MBANS8A1 <seanmbng@aol.com>
2025-11-27 15:23:46 +0000

    The Logo instance has been imported into the PageNav component.

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,8 +1,10 @@
 import { NavLink } from "react-router-dom";
 import styles from "./PageNav.module.css";
+import Logo from "./Logo.jsx";
 function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
       <ul>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
