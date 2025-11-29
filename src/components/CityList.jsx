c329e370e40075b8d853f3d7d43d99bce90f9960
MBANS8A1 <seanmbng@aol.com>
2025-11-29 10:47:27 +0000

    For testing I added the text "city" as content of the CityItem component, and I also added the key for each rendered CityItem on line 9 in CityList.jsx.

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,8 +1,15 @@
+import CityItem from "./CityItem.jsx";
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner.jsx";
 function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
-  return <ul className={styles.CityList}>{cities.map(city=>)}</ul>;
+  return (
+    <ul className={styles.CityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
 }
 
 export default CityList;
