df21f64a5c80d3bd21c0a4d60f8ba6d2a6da2360
MBANS8A1 <seanmbng@aol.com>
2025-11-29 18:08:09 +0000

    I replaced the city references with country references in the CountryList.jsx file; the countries array (empty for now) will be completed from the cities prop, which was received within the CountryList component.

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,7 +1,7 @@
-import CityItem from "./CityItem.jsx";
 import styles from "./CountryList.module.css";
 import Message from "./Message.jsx";
 import Spinner from "./Spinner.jsx";
+import CountryItem from "./CountryItem.jsx";
 function CountryList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
 
@@ -9,10 +9,12 @@ function CountryList({ cities, isLoading }) {
     return (
       <Message message="Add your first city by clicking on a city on the interactive map." />
     );
+
+  const countries = [];
   return (
     <ul className={styles.countrylist}>
-      {cities.map((city) => (
-        <CityItem city={city} key={city.id} />
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.id} />
       ))}
     </ul>
   );
