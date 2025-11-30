0ea3ab8b544e07081419acc6d3a1fe73252006eb
MBANS8A1 <seanmbng@aol.com>
2025-11-30 16:57:49 +0000

    I used the useParam hook, which comes from React Router to destructure the id, which has the id of the city the user will select from the city list. So the state information is grabbed from URL and then I just used it with the h1 element temporary heading for now. Later, I will use this id for getting the specific city information from the fake API using json-server.

--- src/App.jsx
+++ src/App.jsx
@@ -35,9 +35,9 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route index element={<Homepage />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
           <Route
             index
