7b08cc1c13efe04d4bda0d8f7d2a2404a5e106f8
MBANS8A1 <seanmbng@aol.com>
2025-12-02 11:18:33 +0000

    For the nested index Route within the "/app" path, I changed the element prop of it to use Reacr Router DOM's <Navigate/> component to redirect the user to the "/cities" route and tab immediately when the Link button on the Homepage "Start tracking now" is clicked.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Product from "./pages/Product.jsx";
 import Pricing from "./pages/Pricing.jsx";
 import Homepage from "./pages/Homepage.jsx";
@@ -40,10 +40,7 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
         <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
+          <Route index element={<Navigate to="cities" />} />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
