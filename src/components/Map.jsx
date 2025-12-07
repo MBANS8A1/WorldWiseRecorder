682e1aa156283ddc9508497b6a84964baee08cad
MBANS8A1 <seanmbng@aol.com>
2025-12-07 12:55:23 +0000

    I changed the TileLayer url as I wanted the tiles to be thicker and have a better colour scheme and contrast.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -10,12 +10,7 @@ function Map() {
   const lng = searchParams.get("lng");
 
   return (
-    <div
-      className={styles.mapContainer}
-      onClick={() => {
-        navigate("form");
-      }}
-    >
+    <div className={styles.mapContainer}>
       <MapContainer
         center={mapPosition}
         zoom={13}
@@ -24,7 +19,7 @@ function Map() {
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
         />
         <Marker position={mapPosition}>
           <Popup>
