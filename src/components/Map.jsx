dfe57bfb7a1f22d94b662da7c701530f9be4ae28
MBANS8A1 <seanmbng@aol.com>
2025-12-07 23:02:28 +0000

    I am trying to implement the the functionality that will take the user to the form when the interactive map is clicked. To do this I created a DetectMapClick custom component and with leaflet's useMapEvents I want to programmatically navigate to the form using the useNavigate hook. I have started with the click event handler.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,16 @@
 import { useSearchParams, useNavigate } from "react-router-dom";
 import styles from "./Map.module.css";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap,
+  useMapEvents,
+} from "react-leaflet";
 import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesContext";
 function Map() {
-  const navigate = useNavigate();
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]); //[lat,lng]
   const [searchParams] = useSearchParams();
@@ -47,6 +53,14 @@ function Map() {
       </MapContainer>
     </div>
   );
+}
+
+function DetectMapClick() {
+    const navigate = useNavigate();
+
+  useMapEvents({
+    click: (e) => ,
+  });
 }
 
 function ChangeMapCenter({ position }) {
