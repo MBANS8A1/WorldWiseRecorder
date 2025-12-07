e2d0fcc43a26aa1fee516b3d595523856b210135
MBANS8A1 <seanmbng@aol.com>
2025-12-07 12:21:35 +0000

    I took the starter leaflet map code on the react leaflet website and included it within the <div> element with style .mapContainer. Also, I imported the predefined components react leaflet needs.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,6 @@
 import { useSearchParams, useNavigate } from "react-router-dom";
 import styles from "./Map.module.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 function Map() {
   const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
@@ -12,7 +13,20 @@ function Map() {
       onClick={() => {
         navigate("form");
       }}
-    ></div>
+    >
+      {" "}
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 }
 
