f3efece2eb7556d67734b36e0daba018d9348154
MBANS8A1 <seanmbng@aol.com>
2025-12-07 10:08:27 +0000

    In BackButton.jsx, I added the previous logic to create a Button of type "back". Also, I included the useNavigate hook from React Router. Once done, I imported it back into Form.jsx. Now I have a BackButton component, which I can use throughout the application.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import styles from "./Form.module.css";
 import Button from "./Button.jsx";
 import { useNavigate } from "react-router-dom";
+import BackButton from "./BackButton.jsx";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -19,7 +20,6 @@ function Form() {
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
-  const navigate = useNavigate();
   return (
     <form className={styles.form}>
       <div className={styles.row}>
@@ -52,15 +52,7 @@ function Form() {
 
       <div className={styles.buttons}>
         <Button type="primary">Add</Button>
-        <Button
-          type="back"
-          onClick={(e) => {
-            e.preventDefault();
-            navigate(-1);
-          }}
-        >
-          &larr; Back
-        </Button>
+        <BackButton />
       </div>
     </form>
   );
