511951af66e76875ba48de42b023aa7d4e5eca73
MBANS8A1 <seanmbng@aol.com>
2025-12-08 10:42:29 +0000

    I want to add some geolocation features to the map; so I took the custom hook (useGeolocation), which I made in another application earlier and added in a file called useGeolocation.js. This file is in a new folder called "hooks". I need to modify it slightly for this WorldWise application.

---
+++ src/hooks/useGeolocation.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export function useGeolocation() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [position, setPosition] = useState({});
+
+  function getPosition() {
+    if (!navigator.geolocation) {
+      setError("Your browser does not support geolocation!");
+    }
+
+    setIsLoading(true);
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setPosition({
+          lng: position.coords.longitude,
+          lat: position.coords.latitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        isLoading(false);
+      }
+    );
+  }
+  return { error, isLoading, position, getPosition };
+}
