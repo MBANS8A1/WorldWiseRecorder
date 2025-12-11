315ffa95002637691ab390596b436f5df381eeba
MBANS8A1 <seanmbng@aol.com>
2025-12-11 18:27:07 +0000

    I added similar template code in DummyAuthContext.jsx as I did with the CitiesContext.jsx via creating a context, a Provider component and a custom hook from which components can consume the context values passed into the value prop.

---
+++ src/contexts/DummyAuthContext.jsx
@@ -0,0 +1,14 @@
+import { createContext, useContext } from "react";
+
+const AuthContext = createContext();
+
+function AuthProvider({ children }) {
+  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined)
+    throw new Error("AuthContext was used outside of the AuthProvider.");
+  return context;
+}
