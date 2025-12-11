7b94305c21245c9b6e9f0eeab4944e1f13f761b2
MBANS8A1 <seanmbng@aol.com>
2025-12-11 22:43:29 +0000

    I added the initialState object with the user having a value of null and the user not being authenticated.  Also, the reducer as usual will have access to the current state and an action that is dispatched from the functions.

--- src/contexts/DummyAuthContext.jsx
+++ src/contexts/DummyAuthContext.jsx
@@ -1,8 +1,17 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const AuthContext = createContext();
 
+const initialState = {
+  user: null,
+  isAuthenticated: false,
+};
+function reducer(state, action) {}
 function AuthProvider({ children }) {
+  const [{ user, isAuthenticated }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
   function login(email, password) {}
 
   function logout() {}
